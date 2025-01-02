35428890a929be1f9096716ba32c49847edb3567
jonax2008 <jmg_619@hotmail.com>
2025-01-01 22:21:45 -0600

    Agregar funcion de busqueda
    Se agrega funcionde busqueda de acuerdo al texto de cada td

--- src/modules/SearchPanel.jsx
+++ src/modules/SearchPanel.jsx
@@ -1,15 +1,39 @@
-import { DataGrid } from '/src/modules/DataGrid.jsx'
+import { DataGrid } from "/src/modules/DataGrid.jsx";
+// import { useEffect } from "react";
 
+export function SearchPanel() {
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    const rows = document.querySelectorAll(".data-grid tbody tr");
+    rows.forEach((row) => {
+      const columns = row.querySelectorAll("td");
+      let found = false;
+      columns.forEach((column) => {
+        if (column.textContent.toLowerCase().includes(searchValue)) {
+          found = true;
+        }
+      });
+      if (found) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  };
 
-export function SearchPanel(){
-    return (
-        <>    
-            <div id="search" className="search-panel">
-                <input type="search" name="input-search" id="input-search" placeholder='Escriba el nombre para filtrar' className="input-search"/>
-            </div>
-            <DataGrid                 
-                showSelectionPanel={true} 
-            />
-        </>
-    )
+  return (
+    <>
+      <div id="search" className="search-panel">
+        <input
+          type="search"
+          name="input-search"
+          id="input-search"
+          placeholder="Escriba el nombre para filtrar"
+          className="input-search"
+          onInput={handleSearch}
+        />
+      </div>
+      <DataGrid showSelectionPanel={true} />
+    </>
+  );
 }
